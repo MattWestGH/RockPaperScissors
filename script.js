18ba392bd5af3d561fae2c94fea0e8784d4360b9
MattWestGH <workmailmattwest@gmail.com>
2025-03-13 17:42:28 +0200

    Made the game round based and added a score tracker along with a final message to display the score after the game is done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 let rockPaperScissorArr = ["Rock", "Paper", "Scissors"]
+let playerScore = 0;
+let computerScore = 0;
 function getComputerChoice() {
     let randChoice = Math.floor(Math.random() * 3)
     switch (randChoice) {
@@ -18,16 +20,22 @@ function compareChoice(playerChoice) {
     let computerResult = getComputerChoice()
     console.log(computerResult)
     if (playerChoice == "Rock" && computerResult == "Scissors") {
+        playerScore++
         return "You chose rock and the computer chose scissors. You win!"
     } else if (playerChoice == "Scissors" && computerResult == "Paper") {
+        playerScore++
         return "You chose scissors and the computer chose paper. You win!"
     } else if (playerChoice == "Paper" && computerResult == "Rock") {
+        playerScore++
         return "You chose paper and the computer chose rock. You win!"
     } else if (playerChoice == "Rock" && computerResult == "Paper") {
+        computerScore++
         return "You chose rock and the computer chose paper. You lost..."
     } else if (playerChoice == "Paper" && computerResult == "Scissors") {
+        computerScore++
         return "You chose paper and the computer chose scissors. You lost..."
     } else if (playerChoice == "Scissors" && computerResult == "Rock") {
+        computerScore++
         return "You chose scissors and the computer chose rock. You lost..."
     } else if (playerChoice == "Rock" && computerResult == "Rock") {
         return "You both chose rock. It's a draw!"
@@ -44,12 +52,28 @@ function capitalize(word) {
     let result = firstLetterUpper + remainingLetters
     return result
 }
+function nextRound() {
+    let newGame = capitalize(prompt("Would you like to play again?"));
+    if (newGame == "Yes") {
+        startGame();
+    } else if (newGame == "No") {
+        console.log("Final Score:")
+        console.log("Player: " + playerScore);
+        console.log("Computer: " + computerScore);
+        console.log("Thank you for playing!");
+    } else { nextRound() }
+}
 
 function startGame() {
     let playerInput = prompt("Please select rock, paper or scissors.")
     let gameResult = compareChoice(capitalize(playerInput));
-    //console.log(playerInput);
     console.log(gameResult)
+    console.log("Score");
+    console.log("Player: " + playerScore);
+    console.log("Computer: " + computerScore);
+    nextRound();
 }
 
-startGame()
+startGame();
+
+//add funtionality to keep score and make it a round based game
